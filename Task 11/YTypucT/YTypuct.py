8b105174fc3160b55772df037c92c5276276f107
YTypucT <61802122+YTypucT@users.noreply.github.com>
2020-04-16 14:49:30 +0300

    Tasks 11-13 completed

---
+++ Task 11/YTypucT/YTypuct.py
@@ -0,0 +1,9 @@
+for i in range(2):
+    num = int(input())
+    if num == 70:
+        print(70)
+    elif num == 68:
+        print(68)
+    elif num:
+        print (num)
+    else: print ('Ups')
---
+++ Task 12/YTypucT/YTypucT.py
@@ -0,0 +1,10 @@
+num = 69
+str = "it's a string"
+
+if num and str[1] == 't':
+    print ('yep')
+else: print('no')
+if num == 69:
+    if len(str)>1:
+        print ('cool')
+    
